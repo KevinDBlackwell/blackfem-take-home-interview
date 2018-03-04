db320f96061e74ca00fe87c67a9c95650600c81b
Travis Briggs <audiodude@gmail.com>
2018-03-04 13:44:24 -0800

    Add basic model for Links as well as seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+links = Link.create(
+  [{
+     url: 'https://www.youtube.com',
+     title: 'YouTube',
+     description: 'The world\'s largest video sharing site.',
+   }, {
+     url: 'https://news.ycombinator.com',
+     title: 'Hacker News',
+     description: ('A site for programmers and others to discus coding and ' + 
+                   'other technologies.'),
+   }, {
+     url: 'https://en.wikipedia.org/wiki/Main_Page',
+     title: 'English Wikipedia',
+     description: 'The online encyclopedia that anyone can edit, in English',
+   }, {
+     url: 'https://www.blackfem.org/',
+     title: 'BlackFem Homepage',
+     description: ('The homepage of BlackFem Inc, a non-profit dedicated to ' + 
+                   'financial literacy for schoolchildren'),
+   }]
+)
