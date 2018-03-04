16142468e9db731e3ac7718ba7ac08c2439471d7
Travis Briggs <audiodude@gmail.com>
2018-03-04 12:58:45 -0800

    Add first schema migration

---
+++ db/migrate/20180304205433_add_links.rb
@@ -0,0 +1,9 @@
+class AddLinks < ActiveRecord::Migration[5.1]
+  def change
+    create_table :links do |t|
+      t.string :url
+      t.string :title
+      t.text :description
+    end
+  end
+end
