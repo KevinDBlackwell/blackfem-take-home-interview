3a7811d25cbb181a9dda52b16143f7489338cf39
Travis Briggs <audiodude@gmail.com>
2018-03-04 17:01:53 -0800

    Update

--- app/javascript/my_links/app/app.module.ts
+++ app/javascript/my_links/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpModule } from '@angular/http';
+import { CookieXSRFStrategy, HttpModule, XSRFStrategy } from '@angular/http';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
